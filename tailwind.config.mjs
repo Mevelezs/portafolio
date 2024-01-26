cbf60673f508e54bde107b3eb42869473178ee4a
Mauricio Vélez <serketh7@gmail.com>
2024-01-25 23:37:32 -0500

    add social about & projects

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -3,6 +3,9 @@ export default {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
     extend: {
+      gridTemplateColumns: {
+        'GC': 'repeat(auto-fit, minmax(250px, 1fr))',
+      },
       animation: {
         "infinite-scroll": "infinite-scroll 30s linear infinite",
 			},
